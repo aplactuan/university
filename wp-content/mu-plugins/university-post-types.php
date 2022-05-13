b7db6204842da6a1beeaf26e96030d902263b669
Adrian Lactuan <aplactuan@gmail.com>
2022-05-13 14:01:40 +0800

    Campus post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -17,6 +17,24 @@ function university_post_types() {
 		'menu_icon' => 'dashicons-businessperson'
 	]);
 
+    //campus post type
+    register_post_type('campus', [
+        'supports' => ['title', 'editor', 'excerpt'],
+        'rewrite' => ['slug' => 'campuses'],
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => [
+            'name' => 'Campuses',
+            'add_new' => 'Add Campus',
+            'add_new_item' => 'Add New Campus',
+            'edit_item' => 'Edit Campus',
+            'all_items' => 'All Campuses',
+            'singular_name' => 'Campus'
+        ],
+        'menu_icon' => 'dashicons-location-alt'
+    ]);
+
 	//event post type
 	register_post_type('event', [
 		'supports' => ['title', 'editor', 'excerpt'],
--- wp-content/themes/university-theme/functions.php
+++ wp-content/themes/university-theme/functions.php
@@ -69,3 +69,11 @@ function university_adjust_queries($query) {
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+function universityMapKey($api) {
+    $api['key'] = 'AIzaSyCpTDHxpR46BxOz5ow7Qgtxady1uWY3kOc';
+
+    return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'universityMapKey');
