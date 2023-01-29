6701b724e3973a8c6a14dc46e3ee3ce585c0cde9
aplactuan <aplactuan@gmail.com>
2023-01-29 20:52:51 +0800

    Add style to the fake university theme

--- wp-content/themes/fake-university-theme/functions.php
+++ wp-content/themes/fake-university-theme/functions.php
@@ -1,6 +1,7 @@
 <?php
 function fake_university_files() {
-	wp_enqueue_style('fake_university_main_style', get_stylesheet_uri());
+	wp_enqueue_style('fake_university_main_style', get_theme_file_uri('/build/style-index.css'));
+	wp_enqueue_style('fake_university_extra_style', get_theme_file_uri('/build/index.css'));
 }
 
 add_action('wp_enqueue_scripts', 'fake_university_files');
