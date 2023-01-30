d4c5cd7a9d838a6eddaee2bbbce3fbe5f31e5339
aplactuan <aplactuan@gmail.com>
2023-01-30 15:57:44 +0800

    Fake university front page setup

--- wp-content/themes/fake-university-theme/functions.php
+++ wp-content/themes/fake-university-theme/functions.php
@@ -1,5 +1,8 @@
 <?php
 function fake_university_files() {
+	wp_enqueue_script('fake_university_main_script', get_theme_file_uri('/build/index.js'), ['jquery'], 1.0, true);
+	wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+	wp_enqueue_style('custom_google_fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 	wp_enqueue_style('fake_university_main_style', get_theme_file_uri('/build/style-index.css'));
 	wp_enqueue_style('fake_university_extra_style', get_theme_file_uri('/build/index.css'));
 }
