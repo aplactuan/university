5c6ea39a2fd24a354ae10014cc51a366fc843215
aplactuan <aplactuan@gmail.com>
2023-03-27 18:54:47 +0800

    Update professor image on the prescribe sizes

--- wp-content/themes/fake-university-theme/single-program.php
+++ wp-content/themes/fake-university-theme/single-program.php
@@ -47,7 +47,7 @@ while (have_posts()) :
                     ?>
                     <li class="professor-card__list-item">
                         <a href="<?php the_permalink(); ?>" class="professor-card">
-                            <img src="<?php the_post_thumbnail_url(); ?>" class="professor-card__image" >
+                            <img src="<?php the_post_thumbnail_url('professorLandscape'); ?>" class="professor-card__image" >
                             <span class="professor-card__name"><?php the_title(); ?></span>
                         </a>
                     </li>
