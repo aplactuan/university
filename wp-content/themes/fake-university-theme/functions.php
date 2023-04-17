689928337355f9601846382a73e01140afafbd1e
aplactuan <aplactuan@gmail.com>
2023-04-17 19:44:15 +0800

    Adjust archive query for campus archive page

--- wp-content/themes/fake-university-theme/functions.php
+++ wp-content/themes/fake-university-theme/functions.php
@@ -52,6 +52,10 @@ function fake_university_features() {
 add_action('after_setup_theme', 'fake_university_features');
 
 function fake_university_get_posts($query) {
+	if (!is_admin() AND is_post_type_archive('campus') AND $query->is_main_query()) {
+		$query->set('posts_per_page', -1);
+	}
+
 	if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
 		$query->set('order_by', 'title');
 		$query->set('order', 'asc');
