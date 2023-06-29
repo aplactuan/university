4c48ba1917f1c35d4b7125307651d0946de89588
aplactuan <aplactuan@gmail.com>
2023-06-29 15:16:56 +0800

    Add user notes to page with inputs

--- wp-content/themes/fake-university-theme/page-my-notes.php
+++ wp-content/themes/fake-university-theme/page-my-notes.php
@@ -9,7 +9,23 @@ while (have_posts()) :
 	pageBanner();
 	?>
 	<div class="container container--narrow page-section">
-		Our notes content will be here
-	</div>
+		<?php
+            $userNotes = new WP_Query([
+                'post_type' => 'note',
+                'posts_per_page' => -1,
+                'author' => get_current_user_id()
+            ]);
+
+            while ($userNotes->have_posts()) :
+                $userNotes->the_post();
+            ?>
+                <input type="text" class="note-title-field" value="<?php echo esc_attr(get_the_title()) ?>">
+                <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+                <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
+                <textarea class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())) ?></textarea>
+            <?php endwhile;
+            wp_reset_query();
+        ?>
+    </div>
 <?php endwhile;
 get_footer();
