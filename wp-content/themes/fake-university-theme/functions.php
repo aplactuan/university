258bbd3f036868b02c61937196ed2b4925c7217e
aplactuan <aplactuan@gmail.com>
2023-07-31 20:03:41 +0800

    Fix other security issue

--- wp-content/themes/fake-university-theme/functions.php
+++ wp-content/themes/fake-university-theme/functions.php
@@ -144,6 +144,11 @@ function fake_university_login_header_text() {
 
 add_filter('wp_insert_post_data', 'fake_university_make_not_private');
 function fake_university_make_not_private($data) {
+    if ($data['post_type'] == 'note') {
+        $data['post_title'] = sanitize_text_field($data['post_title']);
+        $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    }
+
     if ($data['post_type'] === 'note' && $data['post_status'] !== 'trash') {
         $data['post_status'] = 'private';
     }
--- wp-content/themes/fake-university-theme/page-my-notes.php
+++ wp-content/themes/fake-university-theme/page-my-notes.php
@@ -28,10 +28,10 @@ while (have_posts()) :
 	                $userNotes->the_post();
 	                ?>
                     <li data-id="<?php the_ID(); ?>">
-                        <input type="text" readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()) ?>">
+                        <input type="text" readonly class="note-title-field" value="<?php echo srt_replace('Private: ', '', esc_attr(get_the_title())) ?>">
                         <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i>Edit</span>
                         <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
-                        <textarea readonly class="note-body-field"><?php echo esc_attr(wp_strip_all_tags(get_the_content())) ?></textarea>
+                        <textarea readonly class="note-body-field"><?php echo esc_textarea(wp_strip_all_tags(get_the_content())) ?></textarea>
                         <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i>Edit</span>
                     </li>
                     <?php endwhile; ?>
