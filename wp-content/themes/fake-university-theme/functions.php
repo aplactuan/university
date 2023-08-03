af085304a023d2345438b3587ff75b8fcf2661ef
aplactuan <aplactuan@gmail.com>
2023-08-03 19:50:38 +0800

    Set post limit to each user

--- wp-content/themes/fake-university-theme/functions.php
+++ wp-content/themes/fake-university-theme/functions.php
@@ -142,15 +142,19 @@ function fake_university_login_header_text() {
     return get_bloginfo('name');
 }
 
-add_filter('wp_insert_post_data', 'fake_university_make_not_private');
-function fake_university_make_not_private($data) {
+add_filter('wp_insert_post_data', 'fake_university_make_not_private', 10, 2);
+function fake_university_make_not_private($data, $postArr) {
     if ($data['post_type'] == 'note') {
-        $data['post_title'] = sanitize_text_field($data['post_title']);
-        $data['post_content'] = sanitize_textarea_field($data['post_content']);
-    }
+        if (count_user_posts(get_current_user_id(), 'note') > 4 AND !$postArr['ID']) {
+            die("You have reach your limit");
+        }
 
-    if ($data['post_type'] === 'note' && $data['post_status'] !== 'trash') {
-        $data['post_status'] = 'private';
+	    $data['post_title'] = sanitize_text_field($data['post_title']);
+	    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+
+        if ($data['post_status'] !== 'trash') {
+	        $data['post_status'] = 'private';
+        }
     }
 
     return $data;
